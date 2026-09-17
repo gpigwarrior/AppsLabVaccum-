e2b2ecdfa8415ff648d8c9d41382e2a072e59efd
clayton <cgblue99@gmail.com>
2026-09-17 18:18:33 -0400

    improve drag.m

--- drag.m
+++ drag.m
@@ -1,12 +1,14 @@
 % angle=.301
 % m_b=.04
 % height=8-8*cos(angle)
+clear
+clc
 
 rho=1.22;
-a=pi*0.0399/2;
+A=pi*0.0399/2;
 cd=0.47;
 v_0=200;
-distance=0.22;
+distance=.22;
 v=v_0;
 mass=0.027;
 dt=0.000001;
@@ -14,7 +16,7 @@ t=[0];
 x=[0];
 i=2;
 while x(i-1)<distance
-  fd(i)=-(1/2*rho*(v(i-1))^2*a(i-1)*cd);
+  fd(i)=-(1/2*rho*(v(i-1))^2*A*cd);
   a(i)=fd(i-1)/mass;
   v(i)=v(i-1)+a(i-1)*dt;
   x(i)=x(i-1)+v(i-1)*dt;
@@ -25,4 +27,8 @@ figure(1)
 plot(t,x)
 figure(2)
 plot(x,v)
-disp(v(2)-v(i-1))
+
+delta_v=v(2)-v(i-1);
+energy=1/2*mass*(delta_v^2);
+disp(energy)
+disp(delta_v)
