19cda55bb763049a68771dd579c28a473f0c6057
clayton <cgblue99@gmail.com>
2026-09-17 17:48:38 -0400

    balls.m can read files

--- Balls.m
+++ Balls.m
@@ -6,11 +6,27 @@ mb = 0.00275;
 rho = 1.2; %
 d = 0.04; %
 l = 1.525; %
+r_l=0.2032;
+g=9.81;
 
-theta = input("Input radians: ");
+% Open CSV and read third column into array, then find its maximum
+filename = 'run2.csv';
+Table = readtable(filename);
+angles = Table{:,3};
+pressures = Table{:,2};
 
-v = (mt+mb)/mb*sqrt(2*9.81*(0.2032-0.2032*cos(theta)));
+maxAngle = max(angles);
+minPressure = min(pressures);
 
-v_n = (mt+mb)*sqrt(2*9.81*(0.2032-0.2032*cos(theta)))/(mb+rho*pi/4*d^2*l);
 
+theta = maxAngle
+% theta = input("Input radians: ");
+height=(r_l-r_l*cos(theta))
+v = (mt+mb)/mb*sqrt(2*g*(r_l-r_l*cos(theta)));
+
+v_n = (mt+mb)*sqrt(2*g*(r_l-r_l*cos(theta)))/(mb+rho*pi/4*d^2*l);
+disp("-----------------")
+disp(minPressure)
+disp(v)
+disp(maxAngle)
 fprintf('Exit Velocity = %f [m/s]\nExit Velocity with Air Momentum = %f [m/s]\n', v, v_n)
